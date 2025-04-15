6210e8828de29a0a156dd3933c31740cba949f90
Sujan Dumaru <sdumaru30060@ucumberlands.edu>
2025-04-14 22:23:57 -0400

    Create Task class

---
+++ data-processing-system/src/Task.java
@@ -0,0 +1,16 @@
+// Task class used to set and get data
+public class Task {
+    private String Data;
+
+    public Task(String data) {
+        this.Data = data;
+    }
+
+    public String getData() {
+        return Data;
+    }
+
+    public void setData(String data) {
+        Data = data;
+    }
+}
